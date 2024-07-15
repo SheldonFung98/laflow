311915b3ac7b785e7ed921b3ce5bdb0568363ef3
SheldonVon98 <sheldonvonsr@gmail.com>
2024-07-15 22:35:53 +1000

    Built webpage/
    Branch: main
    Commit: 746c550f25b9e36a8a62d6b98710d1b0d2c4bae6 (dirty)
    
    package:peanut 5.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1869017312"
+    serviceWorkerVersion: "3892541431"
   }
 });
